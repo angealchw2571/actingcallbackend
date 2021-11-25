dc99d5106897d900aef6a7a1f547191a35cc8621
Calvin Sim <calvin_sim222@hotmail.com>
2021-11-25 12:12:05 +0800

    amended datetime

--- forums/models.py
+++ forums/models.py
@@ -3,13 +3,12 @@ from profiles.models import Profiles
 import datetime
 
 now = datetime.datetime.now().replace(microsecond=0)
-nowmod = now.strftime("%Y-%m-%d %H:%M:%S")
 
 class Posts(models.Model):
     title=models.CharField(max_length=200,default="topic-title")
     description=models.CharField(max_length=1000,blank=True)
     postedUser = models.CharField(max_length=100,default="Anonymous")
-    date_created=models.DateTimeField(default=nowmod, null=True)
+    date_created=models.DateTimeField(default=now, null=True)
 
     def __str__(self):
         return str(self.title)
@@ -20,7 +19,7 @@ class Discussion(models.Model):
     commentTitle=models.CharField(max_length=200,default="topic-title")
     comments = models.CharField(max_length=1000)
     commentUser = models.CharField(max_length=100,default="Anonymous")
-    date_created=models.DateTimeField(default=nowmod, null=True)
+    date_created=models.DateTimeField(default=now, null=True)
 
     def __str__(self):
         return str(self.commentTitle)
